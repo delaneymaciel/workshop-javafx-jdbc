73382f003958fbb0e81813fe9f2c6abc495c782d
Delaney Maciel Borges <delaneymaciel@hotmail.com>
2021-06-01 23:48:34 -0300

    Remove department

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -13,6 +13,7 @@ import com.mysql.jdbc.Statement;
 
 import db.DB;
 import db.DbException;
+import db.DbIntegrityException;
 import model.dao.DepartmentDao;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -103,7 +104,7 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 			st.executeUpdate();
 			
 		} catch (SQLException e) {
-			throw new DbException(e.getMessage());
+			throw new DbIntegrityException(e.getMessage());
 		} finally {
 			DB.closeStatement(st);
 		}
