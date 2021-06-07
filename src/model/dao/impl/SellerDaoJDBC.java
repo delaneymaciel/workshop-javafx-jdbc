c215b4d723e48dd724f5ea753d7aafb03cd0d044
Delaney Maciel Borges <delaneymaciel@hotmail.com>
2021-06-06 21:34:29 -0300

    Saving Seller

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -36,7 +36,6 @@ public class SellerDaoJDBC implements SellerDao{
 					+ "VALUES "
 					+ "(?, ?, ?, ?, ?)",
 					Statement.RETURN_GENERATED_KEYS);
-			
 			st.setString(1, obj.getName());
 			st.setString(2, obj.getEmail());
 			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
